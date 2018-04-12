b125d53bd3b641e7ffc52b9cbf58226e627f105c
kuliye <1440894632@qq.com>
2018-04-12 14:57:30 +0800

    Create mysql_simulator.py

---
+++ mysql_simulator.py
@@ -0,0 +1,15 @@
+import pymysql
+charset='utf-8'
+
+connect = pymysql.connect(host='127.0.0.1', user='root', passwd='', db='classs', port=3306)#连接mysql服务器
+
+cur=connect.cursor()#创建数据库指针
+
+while True:
+    order=input("mySQL>")
+    cur.execute("%s"%order)
+    logs=cur.fetchall()
+    for log in logs:
+        print(log)
+
+conct.close()
