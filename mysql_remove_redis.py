337e71074844c360447659da3ade045a4ab94521
kuliye <1440894632@qq.com>
2018-04-15 15:47:45 +0800

    Update mysql_remove_redis.py

--- mysql_remove_redis.py
+++ mysql_remove_redis.py
@@ -2,21 +2,24 @@ import pymysql
 import redis
 class mysqlHelper():
     def __init__(self):
+        """连接mysql数据库并定义指针"""
         self.connection=pymysql.connect(host='127.0.0.1', user='root', passwd='xieliang', db='shoping', port=3306)
         self.cur=self.connection.cursor()
-    def insert(self,name,price,sorted):
-        self.cur.execute("insert into good(name,price,sorted) values('%s',%s,%s)"%(name,price,sorted))
-        self.connection.commit()
+
     def get_menber(self):
+        """"获取值"""
         self.cur.execute("select * from good")
         return self.cur.fetchall()
+    
 class RedisHelper:
     def __init__(self,host='localhost',port=6379):
+        """连接redis数据库"""
         self.redis=redis.StrictRedis(host,port)
-    def hget(self,hash,key):
-        return self.redis.hget(hash,key)
+        
     def hset(self,hash,key,value):
+        """设置值"""
         self.redis.hset(hash,key,value)
+        
 mysqlh=mysqlHelper()
 redish=RedisHelper()
 
